7f3b992e8d4540876b3bdaf3a56b89e92acbc14e
Damian <129460376+iDvmian@users.noreply.github.com>
2025-11-02 17:15:40 -0300

    Update Clases.h

--- Sistema Académico UCM/Clases.h
+++ Sistema Académico UCM/Clases.h
@@ -1,20 +1,26 @@
 #ifndef CLASES_H
 #define CLASES_H
+#include <iostream>
+
 using namespace std;
 
 
+// Clase abstracata Persona y clase padre
 class Persona{
-    public:
+    private:
     string nombre;
     string rut;
     string correo;
 
-// Persona con atributos comunes 
-// como nombre, RUT y correo, y métodos apropiados.
+    public:
     Persona(string n, string r, string c) :  nombre(n), rut(r), correo(c){}
-    virtual ~Persona(){} 
-    virtual void metodo() const = 0;
+    virtual ~Persona(){}
+    // Metodos
     virtual void mostrarInfo() const = 0;
+    string getNombre() const { return nombre; }
+    string getRut() const { return rut; }
+    string getCorreo() const { return correo; }
+
 
     
 };
@@ -24,7 +30,7 @@ class Persona{
 class Profesor : public Persona {
     public:
     Profesor (string n, string r, string c){}
-
+    void mostrarInfo() const override;
 
 };
 
